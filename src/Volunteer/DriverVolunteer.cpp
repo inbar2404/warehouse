4ae52373f2dd5b67d5129742d0d62c685f06f46b
someone <someone@someplace.com>
2024-01-17 15:16:37 +0200

    Add all the requested functions in DriverVolunteer.cpp file - some are with TODO's, means I still have to impliment this

--- src/Volunteer/DriverVolunteer.cpp
+++ src/Volunteer/DriverVolunteer.cpp
@@ -1,1 +1,49 @@
-// TODO: Impliment the class
+#include "../../include/Volunteer.h"  
+
+// TODO: Make sure that init with o the distance left parameter is okay
+DriverVolunteer::DriverVolunteer(int id, string name, int maxDistance, int distancePerStep)
+    : Volunteer(id, name), maxDistance(maxDistance), distancePerStep(distancePerStep), distanceLeft(0) {}
+
+DriverVolunteer* DriverVolunteer::clone() const {
+    // TODO: Make sure my implimentation is okay
+    return new DriverVolunteer(*this);
+}
+
+int DriverVolunteer::getDistanceLeft() const {
+    return distanceLeft;
+}
+
+int DriverVolunteer::getMaxDistance() const {
+    return maxDistance;
+}
+
+int DriverVolunteer::getDistancePerStep() const {
+    return distancePerStep;
+}  
+
+bool DriverVolunteer::decreaseDistanceLeft() {
+    distanceLeft -= distancePerStep;
+    return distanceLeft <= 0;
+}
+
+bool DriverVolunteer::hasOrdersLeft() const {
+    return true;
+}
+
+bool DriverVolunteer::canTakeOrder(const Order &order) const {
+    // TODO: In order to impliment the function I need to find out first how to know if the volunteer can take the order
+    return true; // TODO: Remove this line according to the above comment
+}
+
+void DriverVolunteer::acceptOrder(const Order &order) {
+    // TODO: Prepare for new order(Reset activeOrderId,TimeLeft,DistanceLeft,OrdersLeft depends on the volunteer type)
+}
+
+void DriverVolunteer::step() {
+    // TODO: Impliment the function
+}
+
+string DriverVolunteer::toString() const {
+    // TODO: Understand what type of discription do they want - then impliment
+    return ""; // TODO: Remove this line according to the above comment
+}
