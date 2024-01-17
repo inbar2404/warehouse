1bed15b107016af08b31c99202d6f7bfc6f54b0b
someone <someone@someplace.com>
2024-01-17 14:50:28 +0200

    Add all the requested functions in the CollectorVolunteer.cpp file - some are with TODO's, means I still have to impliment this

--- src/Volunteer/CollectorVolunteer.cpp
+++ src/Volunteer/CollectorVolunteer.cpp
@@ -4,6 +4,15 @@
 CollectorVolunteer::CollectorVolunteer(int id, string name, int coolDown)
     : Volunteer(id, name), coolDown(coolDown), timeLeft(0) {}
 
+CollectorVolunteer* CollectorVolunteer::clone() const {
+    // TODO: Make sure my implimentation is okay
+    return new CollectorVolunteer(*this);
+}
+
+void step() {
+    // TODO: Impliment the function
+}
+
 int CollectorVolunteer::getCoolDown() const {
     return coolDown;
 }
@@ -19,4 +28,20 @@ bool CollectorVolunteer::decreaseCoolDown() {
     return (timeLeft == 0);
 }
 
-// TODO: Impliment the next functions: clone, step, hasOrdersLeft, canTakeOrder, acceptOrder, ToString
+bool CollectorVolunteer::hasOrdersLeft() const {
+    return true;
+}
+
+bool CollectorVolunteer::canTakeOrder(const Order &order) const {
+    // TODO: In order to impliment the function I need to find out first how to know if the volunteer can take the order
+    return true; // TODO: Remove this line according to the above comment
+}
+
+void CollectorVolunteer::acceptOrder(const Order &order) {
+    // TODO: Prepare for new order(Reset activeOrderId,TimeLeft,DistanceLeft,OrdersLeft depends on the volunteer type)
+}
+
+string CollectorVolunteer::toString() const {
+    // TODO: Understand what type of discription do they want - then impliment
+    return ""; // TODO: Remove this line according to the above comment
+}
