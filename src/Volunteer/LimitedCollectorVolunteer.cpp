16e57c4ec38ef7ed371f6f711cc8fcc6a7ad40d6
someone <someone@someplace.com>
2024-01-17 14:59:31 +0200

    Add all the requested functions in thelLimitedCollectorVolunteer.cpp file - some are with TODO's, means I still have to impliment this

--- src/Volunteer/LimitedCollectorVolunteer.cpp
+++ src/Volunteer/LimitedCollectorVolunteer.cpp
@@ -4,6 +4,24 @@
 LimitedCollectorVolunteer::LimitedCollectorVolunteer(int id, string name, int coolDown ,int maxOrders)
     : CollectorVolunteer(id, name, coolDown), maxOrders(maxOrders){}
 
+LimitedCollectorVolunteer* LimitedCollectorVolunteer::clone() const {
+    // TODO: Make sure my implimentation is okay
+    return new LimitedCollectorVolunteer(*this);
+}
+
+bool LimitedCollectorVolunteer::hasOrdersLeft() const {
+    return ordersLeft < maxOrders;
+}
+
+bool LimitedCollectorVolunteer::canTakeOrder(const Order &order) const {
+    // TODO: Find out what exactly they wanted in this question, then - update the line beneath
+    return true;
+}
+
+void LimitedCollectorVolunteer::acceptOrder(const Order &order) {
+    // TODO: Prepare for new order(Reset activeOrderId,TimeLeft,DistanceLeft,OrdersLeft depends on the volunteer type)   
+}
+
 int LimitedCollectorVolunteer::getMaxOrders() const {
     return maxOrders;
 }
@@ -12,4 +30,7 @@ int LimitedCollectorVolunteer::getNumOrdersLeft() const {
     return ordersLeft;
 }
 
-// TODO: Impliment the following functions: clone, hasOrdersLeft, canTakeOrder, acceptOrder, ToString
+string LimitedCollectorVolunteer::toString() const {
+    // TODO: Understand what type of discription do they want - then impliment
+    return ""; // TODO: Remove this line according to the above comment
+}
