43d572896c4f3adca44441ab0ca76460789bf090
someone <someone@someplace.com>
2024-01-27 18:26:01 +0200

    fix ctor

--- src/Customer/SoldierCustomer.cpp
+++ src/Customer/SoldierCustomer.cpp
@@ -1,7 +1,7 @@
 #include "../../include/Customer.h" 
 
-SoldierCustomer(int id, string name, int locationDistance, int maxOrders)
- : customer(id, name, locationDistance, maxOrders) {};
+SoldierCustomer::SoldierCustomer(int id, string name, int locationDistance, int maxOrders)
+ : Customer(id, name, locationDistance, maxOrders) {};
 
 SoldierCustomer* SoldierCustomer::clone() const {
     return new SoldierCustomer(*this);
