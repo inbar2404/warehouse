aaa813310162e9a70f23e3cab97398e12678601e
rotemguetta <rotem10g@gmail.com>
2024-01-30 23:12:23 +0200

    restore warehouse

--- src/Action/RestoreWareHouse.cpp
+++ src/Action/RestoreWareHouse.cpp
@@ -2,28 +2,28 @@
 #include <iostream> 
 extern WareHouse* backup;
 
-// RestoreWareHouse:: RestoreWareHouse(){};
+RestoreWareHouse:: RestoreWareHouse(){};
 
-// RestoreWareHouse* RestoreWareHouse::clone() const{
-//     return new RestoreWareHouse(*this);
-// };
+RestoreWareHouse* RestoreWareHouse::clone() const{
+    return new RestoreWareHouse(*this);
+};
 
-// void RestoreWareHouse::act(WareHouse &wareHouse) {
-//     if(backup == nullptr)
-//     {
-//         error("No backup available");
-//         std::cout << "Error: " + getErrorMsg() << endl;
-//         this->setStatus(ActionStatus::ERROR);
-//     }
+void RestoreWareHouse::act(WareHouse &wareHouse) {
+    if(backup == nullptr)
+    {
+        error("No backup available");
+        std::cout << "Error: " + getErrorMsg() << endl;
+        this->setStatus(ActionStatus::ERROR);
+    }
 
-//     else{
-//         wareHouse = *backup;
-//         this->setStatus(ActionStatus::COMPLETED);
-//     }
-// };
+    else{
+        wareHouse = *backup;
+        this->setStatus(ActionStatus::COMPLETED);
+    }
+};
 
-// string RestoreWareHouse::toString() const {
-//     if(this->getStatus() == ActionStatus::COMPLETED)
-//         return "Backup COMPLETED";
-//     else return "Backup ERROR";
-// };
+string RestoreWareHouse::toString() const {
+    if(this->getStatus() == ActionStatus::COMPLETED)
+        return "Backup COMPLETED";
+    else return "Backup ERROR";
+};
