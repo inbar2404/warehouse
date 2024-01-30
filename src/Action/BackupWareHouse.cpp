8f31196dd704833a55d2cc3abeedd1e203bb7360
rotemguetta <rotem10g@gmail.com>
2024-01-30 23:14:23 +0200

    backup warehouse

--- src/Action/BackupWareHouse.cpp
+++ src/Action/BackupWareHouse.cpp
@@ -1,23 +1,23 @@
 #include "../../include/Action.h" 
 extern WareHouse* backup;
 
-// BackupWareHouse:: BackupWareHouse(){};
+BackupWareHouse:: BackupWareHouse(){};
 
-// BackupWareHouse* BackupWareHouse::clone() const{
-//     return new BackupWareHouse(*this);
-// };
+BackupWareHouse* BackupWareHouse::clone() const{
+    return new BackupWareHouse(*this);
+};
 
-// void BackupWareHouse::act(WareHouse &wareHouse) {
-//     if(backup != nullptr)
-//     {
-//         delete backup;
-//         backup = nullptr;
-//     }
-//     backup = new WareHouse(wareHouse);
-//     wareHouse.addAction(this);
-//     this->setStatus(ActionStatus::COMPLETED);
-// };
+void BackupWareHouse::act(WareHouse &wareHouse) {
+    if(backup != nullptr)
+    {
+        delete backup;
+        backup = nullptr;
+    }
+    backup = new WareHouse(wareHouse);
+    wareHouse.addAction(this);
+    this->setStatus(ActionStatus::COMPLETED);
+};
 
-// string BackupWareHouse::toString() const {
-//     return "Backup COMPLETED";
-// };
+string BackupWareHouse::toString() const {
+    return "Backup COMPLETED";
+};
