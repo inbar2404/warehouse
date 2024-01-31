9656bbb1aa7618cdb0ecfcd24ddd9ab835d4882d
rotemguetta <rotem10g@gmail.com>
2024-01-31 19:18:34 +0200

    close

--- src/Action/Close.cpp
+++ src/Action/Close.cpp
@@ -6,7 +6,13 @@ Close::Close() {}
 
 void Close::act(WareHouse &wareHouse)
 {
+    vector<Order*> orderVec = wareHouse.getVOrders();
     std::stringstream str;
+    for(Order *O : orderVec){
+        str << "OrderID: " << O->getId();
+        str << ", CustomerID: " << O->getCustomerId();
+        str << ", OrderStatus: " << (O->getStatusName()) << std::endl;
+    }
     std::cout << str.str();
     wareHouse.close(); // Close the warehouse and exit main loop
     wareHouse.addAction(this);
@@ -21,5 +27,6 @@ Close *Close::clone() const
 string Close::toString() const
 {
     // ROTEM: I think you should print all orders' logs (look at the running example) -> ant to call it from the act?
+
     return "close COMPLETED";
 }
